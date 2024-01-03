5b94349411be4903fdb492975387dee55791b24e
Mathias <49909074+91maan90@users.noreply.github.com>
2024-01-03 14:10:02 +0100

    Add error message for password mismatch

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 document.getElementById('register-form').addEventListener('submit', validatePasswords);
 
 // Add elements to inputs
-var allInputs = document.querySelectorAll('input');
+const allInputs = document.querySelectorAll('input');
 allInputs.forEach(input => addInputElements(input));
 
 function validatePasswords(event) {
@@ -10,22 +10,26 @@ function validatePasswords(event) {
     event.preventDefault();
 
     // Get the values of password and confirm password inputs
-    let passwordInput = document.getElementById('password');
-    let confirmPasswordInput = document.getElementById('confirm-password');
-    let password = passwordInput.value;
-    let confirmPassword = confirmPasswordInput.value;
+    const passwordInput = document.getElementById('password');
+    const confirmPasswordInput = document.getElementById('confirm-password');
+    const passwordErrorMessage = document.getElementById('pwd-error-msg');
+    const password = passwordInput.value;
+    const confirmPassword = confirmPasswordInput.value;
 
     // Check if the passwords match
     if (password === confirmPassword) {
         // Remove the "error" class if it was previously applied
         passwordInput.classList.remove('error');
         confirmPasswordInput.classList.remove('error');
+        passwordErrorMessage.classList.remove('error');
+
         // Submit the form
         document.getElementById('register-form').submit();
     } else {
         // Apply the "error" class to indicate the error
         passwordInput.classList.add('error');
         confirmPasswordInput.classList.add('error');
+        passwordErrorMessage.classList.add('error');
     }
 }
 
@@ -35,7 +39,7 @@ function addInputElements(input) {
     }
 
     // Create the checkmark-indicator span element
-    var checkmarkIndicator = document.createElement('span');
+    const checkmarkIndicator = document.createElement('span');
     checkmarkIndicator.className = 'checkmark-indicator';
 
     // Insert the span after the input
@@ -44,12 +48,12 @@ function addInputElements(input) {
 
 function addRequiredIndicator(input) {
     // Find the associated label using the 'for' attribute
-    var label = document.querySelector('label[for="' + input.id + '"]');
+    const label = document.querySelector('label[for="' + input.id + '"]');
 
     // Check if a label is found and add the required-indicator span
     if (label) {
         // Create the span element
-        var requiredIndicator = document.createElement('span');
+        const requiredIndicator = document.createElement('span');
         requiredIndicator.className = 'required-indicator';
         requiredIndicator.setAttribute('aria-hidden', 'true');
         requiredIndicator.textContent = '*\u00a0'; // '\u00a0' is the non-breaking space
